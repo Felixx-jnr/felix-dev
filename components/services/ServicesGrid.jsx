d9da63bcc9879aed46b21310ef61559d3d91e94b
Felixx-jnr <uyuoukoh@gmail.com>
2026-07-29 08:43:34 +0100

    feat: update animation duration in ServicesGrid for smoother transitions

--- components/services/ServicesGrid.jsx
+++ components/services/ServicesGrid.jsx
@@ -30,7 +30,7 @@ export default function ServicesGrid() {
           y: 0,
           scale: 1,
           rotate: 0,
-          duration: 0.8,
+          duration: 1,
           stagger: {
             amount: 0.9,
             from: "start",
